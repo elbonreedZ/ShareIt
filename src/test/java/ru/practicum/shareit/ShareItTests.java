5c23bf5fd1f5d260a40014b80cc84086eae32ac3
Елена <elbonreed05@yandex.ru>
2024-11-19 05:57:57 +0300

    Удалила лишнюю аннотацию

--- src/test/java/ru/practicum/shareit/ShareItTests.java
+++ src/test/java/ru/practicum/shareit/ShareItTests.java
@@ -2,12 +2,10 @@ package ru.practicum.shareit;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
-import org.springframework.boot.test.autoconfigure.jdbc.JdbcTest;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 @AutoConfigureTestDatabase
-@JdbcTest
 class ShareItTests {
 
 	@Test
